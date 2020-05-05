0bef4e9917bf9d8444d5dd40d154305a0a83217b
Dennis Ritter <dennis.ritter@beuth-hochschule.de>
2020-05-05 16:52:18 +0200

    feat(motion_matching) Evaluate top 1
    - Evaluating top1 distance matching
    - writing results into file

--- src/mofex/motion_matching.py
+++ src/mofex/motion_matching.py
@@ -19,15 +19,42 @@ def evaluate_matching_top1(train_featvecs: list, val_featvecs: list, make_graph=
     """ Returns a list of estimated most similar motion sequence tuples including feature vectors and respective classes.
         
         Args:
-            train_featvecs (list): A list of tuples, which contain feature vectors and motion classes of a motion image the feature extracting CNN has been trained with (train set).
-            val_featvecs (list): A list of tuples, which contain feature vectors and motion classes of a motion image the feature extracting CNN has been validated with (val/test set).        
+            train_featvecs (list): A list of tuples, which contain id_name, feature vector and motion class of a motion image the feature extracting CNN has been trained with (train set).
+            val_featvecs (list): A list of tuples, which contain id_name, feature vector and motion class of a motion image the feature extracting CNN has been validated with (val/test set).        
+            make_graph (bool): Determine whether to create a result graph or not. (default=False)
+            result_dir_path (bool): Enter a path to store a result file. If None, no result file is created. (default=None) 
     """
-    # for featvec_tuple in val_featvecs:
-    #     id_name = featvec_tuple[0]
-    #     featvec = featvec_tuple[1]
-    #     label = featvec_tuple[2]
 
-    # TODO: Perform top1 matching evaluation
+    # Determine top1 lowest distances by comparing all val_featvecs to all train_featvecs
+    top1_results = []
+    correct = 0
+    incorrect = 0
+    for val_featvec_tuple in val_featvecs:
+        val_id_name = val_featvec_tuple[0]
+        val_featvec = np.array(val_featvec_tuple[1])
+        val_label = val_featvec_tuple[2]
+        distances = []
+        for train_featvec_tuple in train_featvecs:
+            train_id_name = train_featvec_tuple[0]
+            train_featvec = np.array(train_featvec_tuple[1])
+            train_label = train_featvec_tuple[2]
+
+            distances.append(np.linalg.norm(val_featvec - train_featvec))
+        top1_idx = distances.index(min(distances))
+        top1_results.append((val_id_name, val_label, train_featvecs[top1_idx][0], train_featvecs[top1_idx][2]))
+        # print(f'TOP1: {train_featvecs[top1_idx][0]}:\ndist = {distances[top1_idx]}\nlabel = {train_featvecs[top1_idx][2]}')
+        # print('-' * 10)
+        if val_label == train_featvecs[top1_idx][2]:
+            correct += 1
+        else:
+            incorrect += 1
+    print('-' * 10)
+    print(f'Finished Top1 Matching evaluation:')
+    print(f'Items Compared: n_train={len(train_featvecs)}, n_val={len(val_featvecs)}, total_comparisons={len(train_featvecs)*len(val_featvecs)}')
+    print(f'Correct: {correct}/{len(val_featvecs)}')
+    print(f'Incorrect: {incorrect}/{len(val_featvecs)}')
+    print(f'Accuracy: {correct / len(val_featvecs)}')
+    print('-' * 10)
     # TODO: Create Graph and save image if make_graph=True
     # TODO: Write result string
     result = 'add result here'
@@ -37,5 +64,9 @@ def evaluate_matching_top1(train_featvecs: list, val_featvecs: list, make_graph=
             os.makedirs(result_dir_path)
         filename = f'{result_dir_path}/top1_matching_result.txt'
         result_file = open(filename, 'w')
-        result_file.write(result)
+        for result in top1_results:
+            if result[1] == result[3]:
+                result_file.write(f'CORRECT {str(result)}\n')
+            else:
+                result_file.write(f'WRONG {str(result)}\n')
         result_file.close()
