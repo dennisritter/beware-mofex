99f5c8a4ad44a089a999e84aec9303b6ebd101b1
Dennis Ritter <dennis.ritter@beuth-hochschule.de>
2020-05-06 11:48:38 +0200

    fix(dump_motion_images): Ensure each label present

--- src/dump_motion_images_train_val.py
+++ src/dump_motion_images_train_val.py
@@ -2,24 +2,9 @@ import numpy as np
 import cv2
 import json
 import os
-import random
-from datetime import datetime
 from pathlib import Path
 from mofex.preprocessing.sequence import Sequence
-from mofex.preprocessing.skeleton_visualizer import SkeletonVisualizer
-import mofex.models.resnet as resnet
-import torch
 from sklearn.model_selection import train_test_split
-from torchvision import transforms
-""" 
-1. Reads Sequence files
-2. Creates Motion Images
-3. Creates Feature vectors from motion images
-4. Stores Filename(key) Feature Vectors(value) in dict
-5. Saves .JSON file that represents the dict  
-"""
-start = datetime.now()
-
 # Root folder for JSON Sequence files
 # root = 'data/sequences/hdm05-122/c3d'
 root = 'data/sequences/hdm05-122/c3d'
@@ -27,14 +12,25 @@ root = 'data/sequences/hdm05-122/c3d'
 ### Load Sequences
 filenames = []
 sequences = []
+labeled_sequences_dict = {}
 for filename in Path(root).rglob('*.c3d'):
     print(filename)
-    # ! Change the loading function depending on the MoCap format (from_mir_file, from_mka_file, ...)
     name = str(filename).split("\\")[-1]
     desc = str(filename).split("\\")[-2]  # desc = class
-    sequences.append(Sequence.from_hdm05_c3d_file(filename, name=name, desc=desc))
-print(f"Loaded Sequence Files: {datetime.now() - start}")
-train_seqs, val_seqs = train_test_split(sequences, test_size=0.5, random_state=42)
+    # Append sequence to label key or add new label key if not present already
+    # * Change the sequence loading function depending on the MoCap format (from_mir_file, from_mka_file, ...)
+    if desc in labeled_sequences_dict.keys():
+        labeled_sequences_dict[desc].append(Sequence.from_hdm05_c3d_file(filename, name=name, desc=desc))
+    else:
+        labeled_sequences_dict[desc] = [Sequence.from_hdm05_c3d_file(filename, name=name, desc=desc)]
+    # sequences.append(Sequence.from_hdm05_c3d_file(filename, name=name, desc=desc))
+print(f"Loaded Sequence Files")
+train_seqs = []
+val_seqs = []
+for label in labeled_sequences_dict.keys():
+    label_split = train_test_split(labeled_sequences_dict[label], test_size=0.1, random_state=42)
+    train_seqs.extend(label_split[0])
+    val_seqs.extend(label_split[1])
 
 # Train Set
 for seq in train_seqs:
@@ -57,4 +53,4 @@ for seq in val_seqs:
     img = seq.to_motionimg(output_size=(256, 256))
     cv2.imwrite(out, img)
 
-print(f"Created, split and dumped Motion Images: {datetime.now() - start}")
+print(f"Created, split and dumped Motion Images")
