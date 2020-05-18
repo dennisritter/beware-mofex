fix(normalizations): Fixed correct rotation axis

src/mofex/preprocessing/normalizations.py
@@ -11,10 +11,12 @@ def center_positions(positions: 'np.ndarray') -> 'np.ndarray':
         Args: 
             positions (np.ndarray): Positions of a motion sequence of shape  (n_frames, n_body_parts, 3).
     """
-    normalized_positions = np.zeros(positions.shape)
+    # positions -= np.mean(positions, axis=2)
+    # Center Positions by subtracting the mean of each coordinate
     positions[:, :, 0] -= np.mean(positions[:, :, 0])
     positions[:, :, 1] -= np.mean(positions[:, :, 1])
     positions[:, :, 2] -= np.mean(positions[:, :, 2])
+
     return positions
 
 
@@ -53,11 +55,13 @@ def relative_to_positions(positions: 'np.ndarray', root_positions: 'np.ndarray')
 
 def orientation_first_pose_frontal_to_camera(positions: 'np.ndarray', hip_l_idx: int, hip_r_idx: int) -> 'np.ndarray':
     # TODO: Understand why this is the rotation angle. Source: 10.1007/s11042-017-4859-7 chapter 3.2.2
-    # Calc roation angle
-    phi = np.arctan((positions[0, hip_l_idx, 1] - positions[0, hip_r_idx, 1]) / (positions[0, hip_l_idx, 0] - positions[0, hip_r_idx, 0]))
+    # Calc roation angle about Z
+    phi = np.arctan((positions[0, hip_l_idx, 0] - positions[0, hip_r_idx, 0]) / (positions[0, hip_l_idx, 1] - positions[0, hip_r_idx, 1]))
+    rz = np.array([[math.cos(phi), -1 * math.sin(phi), 0], [math.sin(phi), math.cos(phi), 0], [0, 0, 1]])
     # Make array of rotation angles to perform batchwise matrix multiplication
-    r = np.array([[math.cos(phi), 0, math.sin(phi)], [0, 1, 0], [math.sin(phi) * -1, 0, math.cos(phi)]])
-    rotations = np.full((positions.shape[0], 3, 3), r)
+    rotations = np.full((positions.shape[0], 3, 3), rz)
     for bp_idx in range(positions.shape[1]):
-        positions[:, bp_idx] = transformations.mat_mul_batch(positions[:, bp_idx], rotations)
+        for frame_idx in range(positions.shape[0]):
+            positions[frame_idx, bp_idx] = positions[frame_idx, bp_idx] @ rz
+        # positions[:, bp_idx] = transformations.mat_mul_batch(positions[:, bp_idx], rotations)
     return positions