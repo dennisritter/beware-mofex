fix(normalization) Rotation first frame
- Fixed the rotation to represent rotation in correct direction
- Motion Image generation outlier identification with zscore WIP

src/mofex/preprocessing/normalizations.py
@@ -51,7 +51,7 @@ def relative_to_positions(positions: 'np.ndarray', root_positions: 'np.ndarray')
 
 # ! Not Working as expected
 # def orientation_first_pose_frontal_to_camera(positions: 'np.ndarray', hip_l_idx: int, hip_r_idx: int) -> 'np.ndarray':
-#     # TODO: Understand why this is the rotation angle. Source: 10.1007/s11042-017-4859-7 chapter 3.2.2
+#     # TODO: Understand why this is the rotation angle. Source: 10.1007/s11042-017-4859-7 chapter 3.2.2 chapter 3.2.2
 #     # Calc roation angle about Z
 #     phi = np.arctan((positions[0, hip_l_idx, 0] - positions[0, hip_r_idx, 0]) / (positions[0, hip_l_idx, 1] - positions[0, hip_r_idx, 1]))
 #     rz = np.array([[math.cos(phi), -1 * math.sin(phi), 0], [math.sin(phi), math.cos(phi), 0], [0, 0, 1]])
@@ -76,7 +76,6 @@ def orientation_first_pose_frontal_to_camera(positions: 'np.ndarray', hip_l_idx:
     # print(f'PHI = {phi}')
 
     rz = transformations.get_rotation(vec_hip_l_to_hip_r, vec_x_axis)[:3, :3]
-    print(rz)
     # rz = np.array([[math.cos(phi), -1 * math.sin(phi), 0], [math.sin(phi), math.cos(phi), 0], [0, 0, 1]])
     # # Make array of rotation angles to perform batchwise matrix multiplication
     # rotations = np.full((positions.shape[0], 3, 3), rz)

src/mofex/preprocessing/transformations.py
@@ -26,17 +26,17 @@ def get_angle_batch(vec1, vec2):
     return np.arccos(dot_batch(vec1, vec2))
 
 
-def get_rotation(vec1, vec2):
+def get_rotation(vec_from, vec_to):
     """Returns a homogenious 4x4 transformation matrix without translation vector that describes the rotational transformation from vec1 to vec2
 
     Args:
-        vec1 (np.ndarray): A 3-D vector whose direction defines the starting point of rotation.
-        vec2 (np.ndarray): A 3-D vectors whose direction defines the end point of rotation.
+        vec_from (np.ndarray): A 3-D vector whose direction defines the starting point of rotation.
+        vec_to (np.ndarray): A 3-D vectors whose direction defines the end point of rotation.
     """
-    vec1 = norm(vec1)
-    vec2 = norm(vec2)
-    theta = get_angle(vec1, vec2)
-    rotation_axis = get_perpendicular_vector(vec1, vec2)
+    vec_from = norm(vec_from)
+    vec_to = norm(vec_to)
+    theta = get_angle(vec_to, vec_from)
+    rotation_axis = get_perpendicular_vector(vec_to, vec_from)
     rot_mat = rotation_matrix_4x4(rotation_axis, theta)
     return rot_mat
 

src/plot_coordinate_historgram.py
@@ -16,7 +16,7 @@ import mofex.feature_vectors as featvec
 import plotly.graph_objects as go
 from mofex.load_sequences import load_seqs_asf_amc
 
-root = './data/sequences/hdm05-122/amc/walkRightCircle4StepsRstart'
+root = './data/sequences/hdm05-3/amc/'
 filename_asf = '*.asf'
 filename_amc = '*.amc'
 seqs = load_seqs_asf_amc(root, filename_asf, filename_amc)
@@ -24,53 +24,55 @@ seqs = load_seqs_asf_amc(root, filename_asf, filename_amc)
 x = []
 y = []
 z = []
+
 for seq in seqs:
     print(f'Processing: {seq.name}')
     # Normalization
     seq.norm_center_positions()
     seq.norm_relative_to_positions((seq.positions[:, 1, :] + seq.positions[:, 6, :]) * 0.5)
+    # sv_orig = SkeletonVisualizer(seq)
+    # sv_orig.show()
     seq.norm_orientation_first_pose_frontal_to_camera(1, 6)
     # Add flattened xyz values to list respectively
-#     x.extend(seq.positions[:, :, 0].flatten().tolist())
-#     y.extend(seq.positions[:, :, 1].flatten().tolist())
-#     z.extend(seq.positions[:, :, 2].flatten().tolist())
+    x.extend(seq.positions[:, :, 0].flatten().tolist())
+    y.extend(seq.positions[:, :, 1].flatten().tolist())
+    z.extend(seq.positions[:, :, 2].flatten().tolist())
 
-# # make array from list
-# x = np.array(x)
-# y = np.array(y)
-# z = np.array(z)
-# # sort
-# x.sort()
-# y.sort()
-# z.sort()
-# # Get overall min/max values for xyz respectively
-# # ignore outer 1%
-# xmin = math.floor(x[math.floor(0.01 * len(x)):].min())
-# xmax = math.ceil(x[:math.floor(len(x) - 0.01 * len(x))].max())
-# ymin = math.floor(y[math.floor(0.01 * len(y)):].min())
-# ymax = math.ceil(y[:math.floor(len(y) - 0.01 * len(y))].max())
-# zmin = math.floor(z[math.floor(0.01 * len(z)):].min())
-# zmax = math.ceil(z[:math.floor(len(z) - 0.01 * len(z))].max())
+# make array from list
+x = np.array(x)
+y = np.array(y)
+z = np.array(z)
+# sort
+x.sort()
+y.sort()
+z.sort()
+# Get overall min/max values for xyz respectively
+# ignore outer 1%
+xmin = math.floor(x[math.floor(0.01 * len(x)):].min())
+xmax = math.ceil(x[:math.floor(len(x) - 0.01 * len(x))].max())
+ymin = math.floor(y[math.floor(0.01 * len(y)):].min())
+ymax = math.ceil(y[:math.floor(len(y) - 0.01 * len(y))].max())
+zmin = math.floor(z[math.floor(0.01 * len(z)):].min())
+zmax = math.ceil(z[:math.floor(len(z) - 0.01 * len(z))].max())
 
-# xgroups = []
-# ygroups = []
-# zgroups = []
-# for min_rng in range(xmin, xmax):
-#     xgroups.append(x[np.where((x > min_rng) & (x < min_rng + 1))[0]])
-# for min_rng in range(ymin, ymax):
-#     ygroups.append(y[np.where((y > min_rng) & (y < min_rng + 1))[0]])
-# for min_rng in range(zmin, zmax):
-#     zgroups.append(z[np.where((z > min_rng) & (z < min_rng + 1))[0]])
-# xgroup_sizes = [len(group) for group in xgroups]
-# ygroup_sizes = [len(group) for group in ygroups]
-# zgroup_sizes = [len(group) for group in zgroups]
+xgroups = []
+ygroups = []
+zgroups = []
+for min_rng in range(xmin, xmax):
+    xgroups.append(x[np.where((x > min_rng) & (x < min_rng + 1))[0]])
+for min_rng in range(ymin, ymax):
+    ygroups.append(y[np.where((y > min_rng) & (y < min_rng + 1))[0]])
+for min_rng in range(zmin, zmax):
+    zgroups.append(z[np.where((z > min_rng) & (z < min_rng + 1))[0]])
+xgroup_sizes = [len(group) for group in xgroups]
+ygroup_sizes = [len(group) for group in ygroups]
+zgroup_sizes = [len(group) for group in zgroups]
 
-# min = np.array([xmin, ymin, zmin]).min()
-# max = np.array([xmax, ymax, zmax]).max()
-# group_labels = [f'[{g},{(g+1)}]' for g in range(min, max)]
-# fig = go.Figure(data=[go.Bar(x=group_labels, y=xgroup_sizes), go.Bar(x=group_labels, y=ygroup_sizes), go.Bar(x=group_labels, y=zgroup_sizes)])
-# fig.update_layout(barmode='group')
-# fig.show()
-
-sv = SkeletonVisualizer(seqs[0])
-sv.show()
+min = np.array([xmin, ymin, zmin]).min()
+max = np.array([xmax, ymax, zmax]).max()
+group_labels = [f'[{g},{(g+1)}]' for g in range(min, max)]
+fig = go.Figure(data=[go.Bar(x=group_labels, y=xgroup_sizes), go.Bar(x=group_labels, y=ygroup_sizes), go.Bar(x=group_labels, y=zgroup_sizes)])
+fig.update_layout(barmode='group')
+fig.show()
+# sv = SkeletonVisualizer(seqs[02])
+# sv.show()