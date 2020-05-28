6606a0a24a52052b28b2792f7b34bf4b85613750
Dennis Ritter <dennis.ritter@beuth-hochschule.de>
2020-05-28 19:07:10 +0200

    feat(plot histogram): Updated script
    - Applied new orientation norm
    - tested some outlier filtering methods and params

--- src/dump_motion_images_train_val.py
+++ src/dump_motion_images_train_val.py
@@ -15,7 +15,15 @@ filename_asf = '*.asf'
 filename_amc = '*.amc'
 
 dump_root = 'data/motion_images'
-dataset_name = 'hdm05-3_90-10'
+dataset_name = 'hdm05-122_90-10'
+
+# Indices constants for body parts that define normalized orientation of the skeleton
+# left -> hip_left
+LEFT_IDX = 1
+# right -> hip_right
+RIGHT_IDX = 6
+# up -> lowerback
+UP_IDX = 11
 
 # --- Loading Sequences and preprocessing
 seqs = load_seqs_asf_amc(src_root, filename_asf, filename_amc)
@@ -30,8 +38,8 @@ for seq in seqs:
     # Normalization
     # * Body Part Indices -> 1 = left hip; 6 = right hip
     seq.norm_center_positions()
-    seq.norm_relative_to_positions((seq.positions[:, 1, :] + seq.positions[:, 6, :]) * 0.5)
-    seq.norm_orientation_first_pose_frontal_to_camera(1, 6)
+    seq.norm_relative_to_positions((seq.positions[:, LEFT_IDX, :] + seq.positions[:, RIGHT_IDX, :]) * 0.5)
+    seq.norm_orientation(seq.positions[0, LEFT_IDX], seq.positions[0, RIGHT_IDX], seq.positions[0, UP_IDX])
     # Add flattened xyz values to list respectively
     x.extend(seq.positions[:, :, 0].flatten().tolist())
     y.extend(seq.positions[:, :, 1].flatten().tolist())
@@ -57,21 +65,55 @@ z = np.array(z)
 x.sort()
 y.sort()
 z.sort()
-# Get overall min/max values for xyz respectively
-# ignore outer 1%
-xmin = x[math.floor(0.01 * len(x)):].min()
-xmax = x[:math.floor(len(x) - 0.01 * len(x))].max()
-ymin = y[math.floor(0.01 * len(y)):].min()
-ymax = y[:math.floor(len(y) - 0.01 * len(y))].max()
-zmin = z[math.floor(0.01 * len(z)):].min()
-zmax = z[:math.floor(len(z) - 0.01 * len(z))].max()
-minmax_filename = f'{dump_root}/{dataset_name}/minmax_values.txt'
 
+
+# Get overall min/max values for xyz respectively
+# xmin = math.floor(x.min())
+# xmax = math.ceil(x.max())
+# ymin = math.floor(y.min())
+# ymax = math.ceil(y.max())
+# zmin = math.floor(z.min())
+# zmax = math.ceil(z.max())
+# Ignore outer 1%
+# x = x[math.floor(0.01 * len(x)):math.floor(len(x) - 0.01 * len(x))]
+# y = y[math.floor(0.01 * len(y)):math.floor(len(y) - 0.01 * len(y))]
+# z = z[math.floor(0.01 * len(z)):math.floor(len(z) - 0.01 * len(z))]
+# Z Score outlier filtering
+# ZSCORE_ABS_THRESHOLD = 2.5
+# x = x[abs(stats.zscore(x)) < 3]
+# y = y[abs(stats.zscore(y)) < 3]
+# z = z[abs(stats.zscore(z)) < 2.5]
+# IQR outlier filtering
+def filter_outliers_iqr(data: 'np.ndarray', factor: float = 1.5):
+    q1 = np.quantile(x, 0.25)
+    q3 = np.quantile(x, 0.75)
+    iqr = q3 - q1
+    lower = q1 - factor * iqr
+    upper = q3 + factor * iqr
+    return data[np.where((data > lower) & (data < upper))]
+
+
+x_iqr_factor = 2.5
+y_iqr_factor = 2.5
+z_iqr_factor = 3.5
+x = filter_outliers_iqr(x, factor=2.5)
+y = filter_outliers_iqr(y, factor=2.5)
+z = filter_outliers_iqr(z, factor=3.5)
+
+xmin = x.min()
+xmax = x.max()
+ymin = y.min()
+ymax = y.max()
+zmin = z.min()
+zmax = z.max()
+minmax_filename = f'{dump_root}/{dataset_name}/minmax_values.txt'
 # Create basic text file to store(remember) the used min/max values
 if not os.path.isdir(f'{dump_root}/{dataset_name}'):
     os.makedirs(f'{dump_root}/{dataset_name}')
 minmax_file = open(minmax_filename, 'w')
 minmax_file.write(f'x: [{xmin}, {xmax}]\ny: [{ymin}, {ymax}]\nz: [{zmin}, {zmax}]')
+minmax_file.write(f'\n')
+minmax_file.write(f'x_iqr_factor: {x_iqr_factor}\ny_iqr_factor: {y_iqr_factor}\nz_iqr_factor: {z_iqr_factor}')
 minmax_file.close()
 
 # --- Create Motion Images
@@ -103,18 +145,16 @@ for seq in val_seqs:
 xgroups = []
 ygroups = []
 zgroups = []
-for min_rng in range(math.floor(xmin), math.ceil(xmax)):
+min = np.array([math.floor(xmin), math.floor(ymin), math.floor(zmin)]).min()
+max = np.array([math.ceil(xmax), math.ceil(ymax), math.ceil(zmax)]).max()
+for min_rng in range(min, max):
     xgroups.append(x[np.where((x > min_rng) & (x < min_rng + 1))[0]])
-for min_rng in range(math.floor(ymin), math.ceil(ymax)):
     ygroups.append(y[np.where((y > min_rng) & (y < min_rng + 1))[0]])
-for min_rng in range(math.floor(zmin), math.ceil(zmax)):
     zgroups.append(z[np.where((z > min_rng) & (z < min_rng + 1))[0]])
 xgroup_sizes = [len(group) for group in xgroups]
 ygroup_sizes = [len(group) for group in ygroups]
 zgroup_sizes = [len(group) for group in zgroups]
 
-min = np.array([math.floor(xmin), math.floor(ymin), math.floor(zmin)]).min()
-max = np.array([math.ceil(xmax), math.ceil(ymax), math.ceil(zmax)]).max()
 group_labels = [f'[{g},{(g+1)}]' for g in range(min, max)]
 fig = go.Figure(data=[
     go.Bar(name='X Coords', x=group_labels, y=xgroup_sizes),
