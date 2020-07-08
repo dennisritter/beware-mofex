5943ac58345e67408cb994beb9bcdcdf22194bc3
Dennis Ritter <dennis.ritter@beuth-hochschule.de>
2020-07-08 20:22:43 +0200

    feat(reps in stream): Stream + reps counting WIP

--- src/motion_stream.py
+++ src/motion_stream.py
@@ -16,6 +16,9 @@ from mofex.preprocessing.skeleton_visualizer import SkeletonVisualizer
 import mofex.preprocessing.normalizations as norm
 import mofex.feature_vectors as feature_vectors
 from mofex.load_sequences import load_seqs_asf_amc_hdm05
+import locate_reps
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 # ! Root directory of motion images. Make sure it includes 'train' and 'val' directory, which then include a directory for each present label in the dataset.
 # ! Structure template: <in_path>/train/<class_name>/motion_img1.png
@@ -37,7 +40,12 @@ preprocess = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-# Root folder for Sequence files
+# Load Ground Truth Sequence
+asf_path = './data/sequences/hdm05-122/amc/squat1Reps/HDM_bd.asf'
+amc_path = './data/sequences/hdm05-122/amc/squat1Reps/HDM_bd_squat1Reps_001_120.amc'
+seq_gt = Sequence.from_hdm05_asf_amc_files(asf_path, amc_path)
+
+# Root folder for Query Sequence files
 filename_asf = '*.asf'
 filename_amc = '*.amc'
 src_root = './data/sequences/hdm05-122/amc/squat3Reps/'
@@ -50,7 +58,7 @@ print(f'..Created query sequence of {len(seq_q)} frames.')
 
 def fill_queue(queue: Queue, batchsize: int = 10):
     """ Just a helper function to fill the queue with sequence batches"""
-    seq_q_batches = seq.split_i.split(overlap=0.0, subseq_size=batchsize)
+    seq_q_batches = seq_q.split(overlap=0.0, subseq_size=batchsize)
     for batch in seq_q_batches:
         queue.put(batch)
     return queue
@@ -61,9 +69,7 @@ def fill_queue(queue: Queue, batchsize: int = 10):
 @click.option("--fps", default=120, help="How many frames per second do you want to stream?")
 @click.option("--delay", default=0, help="How much delay you want to add between batches?")
 def stream(batchsize, fps, delay):
-    state = {
-        "paused": False,
-    }
+    state = {"pointer": 0, "counted": 0, "counting_rate": fps}
     click.echo(f'Simulating motion stream:\nFPS: {fps}\nBatchSize: {batchsize}')
     click.echo(f'Sending a batch of [{batchsize}] frames every [{(batchsize / fps):.2f}] seconds...')
     t = 0
@@ -72,9 +78,8 @@ def stream(batchsize, fps, delay):
 
     # Init seq_q, the stitched query sequence
     seq_q = None
-    distances = []
     keyframes = []
-    seqs_q_single_rep = []
+    local_minima, distances = [], []
     while True:
         time.sleep((batchsize / fps) + delay)
         t += batchsize / fps
@@ -82,6 +87,12 @@ def stream(batchsize, fps, delay):
 
         # Handle empty queue
         if seq_q_queue.empty():
+            print(f'Stream stopped...')
+            print(f'--- RESULT ---')
+            print(f'Repititions Counted: {len(keyframes)} ')
+            print(F'--------------')
+
+            # refill queue and start again
             seq_q = None
             seq_q_queue = fill_queue(Queue(maxsize=0))
         # Create query sequence if it is None
@@ -99,34 +110,24 @@ def stream(batchsize, fps, delay):
         else:
             seq_q.append(seq_q_queue.get())
 
-        # * Normalize the query sequence in a copy
-        # * Otherwise we normalize multiple times, which we shouldn't
-        seq_q_normed = seq_q[:]
-        print(f'Q LENGTH: {len(seq_q_normed)}')
-        # seq_q_normed.norm_center_positions()
-        # ! 30 and 37 are position indices somewhere near left/right hip.
-        # ! Adjust if using other body part model! (currently for hdm05 dataset sequences)
-        # seq_q_normed.norm_relative_to_positions((seq_q_normed.positions[:, 30, :] + seq_q_normed.positions[:, 37, :]) * 0.5)
-        # seq_q_normed.norm_orientation_first_pose_frontal_to_camera(30, 37)
-        # TODO: Remove debug visualization
-        # sv = SkeletonVisualizer(seq_q_normed)
-        # sv.show()
-        # click.pause()
-
-        # Make motion image from sequence
-        # seq_q_normed.to_motionimg()
-        # Make feature vector from motion image
-        featvec_q = feature_vectors.load_from_sequences([seq_q_normed], model, preprocess, feature_size)[0]
-
-        distance = np.linalg.norm(featvec_q - featvec_gt)
-        distances.append(distance)
-        minima = argrelextrema(np.array(distances), np.less, order=5)[0]
-        if len(minima >= 1):
-            print(f'I found a distance minimum after [{(minima[0] + 1) * batchsize}] frames.')
-            keyframes.append((minima[0] + 1) * batchsize)
-            seqs_q_single_rep.append(seq_gt_normed[0:minima[0]])
-            seq_q = seq_q[minima[0]:]
-            distances = distances[minima[0]:]
+        ### Do whatever you want to do here...
+        # Count Repetitions
+        if len(seq_q) >= state['counting_rate'] * (1 + state['counted']):
+            start = state['counting_rate'] * state['counted']
+            stop = start + state['counting_rate']
+            result = locate_reps.locate_reps(seq_q[start:stop], seq_gt, model=model, feature_size=feature_size, preprocess=preprocess)
+            local_minima = np.concatenate((local_minima, result[0]))
+            distances = np.concatenate((distances, result[1]))
+            for minimum in local_minima:
+                keyframes.append(start + minimum)
+            state['counted'] += 1
+            print(f'Analyzed {stop} Frames')
+            print(f'Keyframes: {keyframes}')
+        if len(seq_q) == 10 * state['counting_rate']:
+            fig = make_subplots(rows=1, cols=1)
+            fig.append_trace(go.Scatter(x=np.arange(len(distances)), y=distances, name=f'distances'), row=1, col=1)
+            fig.update_layout(height=500, width=1500, title_text="Savgol Smoothed distances and key markings. (3 Squat Repetition Sequence)")
+            fig.show()
 
 
 if __name__ == '__main__':
