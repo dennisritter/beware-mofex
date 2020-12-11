c4efa76f03cb84acc284b4087b0408ba7e59daa9
Christopher Kümmel <kuemmel.christopher@gmail.com>
2020-12-11 14:25:21 +0100

    feat(utils): print optimizer
    - Add optimizer str to path name for model plotter and saver

--- src/mofex/model_plotter.py
+++ src/mofex/model_plotter.py
@@ -4,20 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_val_acc_on_batch(val_accuracies, model_name, dataset_name, num_pretrained_epochs=0, path='data', show=False, save=True):
+def plot_val_acc_on_batch(val_accuracies, model_name, dataset_name, optimizer, num_pretrained_epochs=0, path='data', show=False, save=True):
     """ Plots the validation accuracies on batch number. 
 
         Args:
             val_accuracies (list): A list of accuracy values to plot. Reach value represents the result of one epoch.
             model_name (str): The name of the trained model that is used in the file name.
             dataset_name (str): The name of the dataset used for training that is used in the file name.
+            optimizer(str): The name of the optimizer.
             num_pretrained_epochs (int): The number of epochs this model has been trained before.
             path (str): The path where the figure saved
             show (bool): Show the plot or not
             save (bool): Save plot on disk or not
     """
     num_epochs = len(val_accuracies) + num_pretrained_epochs
-    plot_name = f'{model_name}_{dataset_name}_e{num_pretrained_epochs}-{num_epochs}'
+    plot_name = f'{model_name}_{dataset_name}_{optimizer}_e{num_pretrained_epochs}-{num_epochs}'
     plt.title(f'Val Acc vs. Epochs for {plot_name}')
     plt.xlabel("Epochs")
     plt.ylabel("Accuracy")
--- src/mofex/model_saver.py
+++ src/mofex/model_saver.py
@@ -3,7 +3,7 @@ import os
 import torch
 
 
-def save_model(model, save_dir, model_name, dataset_name, num_epoch):
+def save_model(model, save_dir, model_name, dataset_name, num_epoch, optimizer):
     """ Saves a models state to disc.
 
         Args:
@@ -11,13 +11,14 @@ def save_model(model, save_dir, model_name, dataset_name, num_epoch):
             save_dir: save model direction
             model_name:  model name
             epoch:  epoch
+            optimizer: name of optimizer
     """
     # Create Folder(s) in path if doesn't exist already
     if not os.path.isdir(save_dir):
         os.makedirs(save_dir)
     save_path = os.path.join(
         save_dir,
-        f'{model_name}_{dataset_name}_e{num_epoch}.pt',
+        f'{model_name}_{dataset_name}_{optimizer}_e{num_epoch}.pt',
     )
     output_path = open(save_path, mode="wb")
     torch.save(model.state_dict(), output_path)
