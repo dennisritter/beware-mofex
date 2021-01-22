81d2d1bbe2f76b2625cadb7e872e207b71317e96
Christopher <christopher.kuemmel@beuth-hochschule.de>
2021-01-22 14:00:26 +0100

    feat(scripts): mka dropout set
    - Add script to create class-balanced subset of mka cookie

---
+++ scripts/evaluation/mka-beware-downstream-dropout.py
@@ -0,0 +1,62 @@
+"""The generated cookie downstream dataset has a class imbalance.
+85% of noreps and 15% of reps. Therefore, this script generates sub-sets with
+desired class ratios.
+
+
+MKA beware 1.1 cookie dataset:
+
+    train:
+        norep:  # 603518 files
+        rep:    # 108599 files
+
+    val:
+        norep:  # 67059 files
+        rep:    # 12642 files
+"""
+
+import glob
+import os
+import random
+import shutil
+
+random.seed(42)
+
+
+def create_dropout_set(percent: float):
+    """
+    Args:
+        prob (float): The percentage to use from the norep distribution.
+    """
+    print('------------------------')
+    print(f'Start creating dropout {percent} set.')
+    for _type in ['motion_images', 'sequence_chunks']:
+        for _set in ['train', 'val']:
+            print(f'.. creating {_type} - {_set} dataset.')
+            os.makedirs('data/mka-beware-1.1/' + _type +
+                        '/mka-beware-1.1_cookie_dropout-' + str(percent) + '/' +
+                        _set + '/norep',
+                        exist_ok=True)
+            for idx, _file in enumerate(
+                    sorted(
+                        list(
+                            glob.glob('data/mka-beware-1.1/' + _type +
+                                      '/mka-beware-1.1_cookie/' + _set +
+                                      '/norep/*')))):
+                if idx % 1000 == 0:
+                    print(f'.. processed: {idx}')
+
+                # skip 1 - percentage frames
+                if not random.random() < percent:
+                    continue
+
+                shutil.copy(
+                    _file, 'data/mka-beware-1.1/' + _type +
+                    '/mka-beware-1.1_cookie_dropout-' + str(percent) + '/' +
+                    _set + '/norep')
+    print('DONE')
+    print('------------------------')
+
+
+if __name__ == "__main__":
+    create_dropout_set(0.2)
+    create_dropout_set(0.4)
